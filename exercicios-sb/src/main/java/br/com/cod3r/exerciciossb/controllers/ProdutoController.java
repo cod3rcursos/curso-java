d9d7c80c59453d24dabf2b95b21d212f0bbaf3f1
Leonardo Moura Leitão <leonardomleitao@gmail.com>
2020-12-10 16:53:16 -0300

    Exercicios de Spring Boot

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,10 +1,17 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +21,49 @@ import br.com.cod3r.exerciciossb.model.repositories.ProdutoRepository;
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
 
-	@PostMapping
-	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
+//	@PostMapping
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.PUT })
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
+	}
+
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+
+	@GetMapping(path = "/nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+//		return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+		return produtoRepository.searchByNameLike(parteNome);
+	}
+
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+	public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
+		if (qtdePagina >= 5)
+			qtdePagina = 5;
+		Pageable page = PageRequest.of(numeroPagina, qtdePagina);
+		return produtoRepository.findAll(page);
+	}
+
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+
+//	@PutMapping
+//	public Produto alterarProduto(@Valid Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
+
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
 	}
 }
--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,10 +1,24 @@
 package br.com.cod3r.exerciciossb.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.cod3r.exerciciossb.model.entities.Produto;
 
-public interface ProdutoRepository
-	extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+
+//	findByNomeContaining
+//	findByNomeIsContaining
+//	findByNomeContains
+//	
+//	findByNomeStartsWith
+//	findByNomeEndsWith
+//	
+//	findByNomeNotContaining
+
+	@Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
 }
